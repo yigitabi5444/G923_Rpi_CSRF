2d07dea761ff2bf461b79e55ff43b53cfd047864
Yiğit <yigit.kilicarslan@outlook.com>
2023-12-16 10:53:24 +0300

    asd

--- script.py
+++ script.py
@@ -32,7 +32,7 @@ except ImportError as e:
 def main():
     pygame.init()
     try:
-        ser = serial.Serial('dev/serial1',
+        ser = serial.Serial('/dev/ttyAMA0',
                         baudrate=115000,
                         parity=serial.PARITY_NONE,
                         stopbits=serial.STOPBITS_ONE)
